5b6a85d51e4eacd8550006ea167aaad49b9e87f5
nicotrixxel <104150729+nicotrixxel@users.noreply.github.com>
2023-05-09 21:32:06 +0200

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    
+    // This is a comment
     printf("Hello World\n");
-    printf("I like pizza");
+    printf("\"I like pizza\" - Nico\n");
 
+    int x; //declaration
+    x = 123;
+    int y = 234;
+    int age;
+    float gpa = 2.04;
+    char name[25];
+
+    printf("What's your name?");
+    fgets(name, 25, stdin);
+    printf("\nHello %s!\n", name);
+    printf("How old are you? ");
+    scanf("%d", &age);
+    printf("\n You are %d years old \n", age);
     return 0;
 }
