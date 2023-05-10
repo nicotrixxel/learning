d077e0ece4d4ec23238827e44aa809d6934872fe
nicotrixxel <104150729+nicotrixxel@users.noreply.github.com>
2023-05-10 18:44:30 +0200

    Update function.c

--- function.c
+++ function.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    function();
-    printf("main\n");
+    int a = 42;
+    int b = 1337;
+    int result = berechnung(a, b); //result is a
+    printf("main %d\n", result);
     return 0;
 }
 
-int function() {
-    printf("function\n");
-    function2();
-}
-
-int function2() {
-    printf("function2\n");
+int berechnung(int x, int y) {
+    int ret;
+    if(x > y) {
+        ret = x;
+    }
+    else {
+        ret = y;
+    }
+    return ret;
 }
